a25874c910ee7a97eefceed8ae4a7352753c927f
William Mullins <twfdisturbed@hotmail.com>
2018-07-09 09:51:59 -0400

    Added four more test

--- tdd_test.rb
+++ tdd_test.rb
@@ -6,8 +6,13 @@ class TestCodes < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
-
     def test_to_return_string
         assert_equal(String, string_return("Coded").class)
+        assert_equal(String, string_return("Coded with spaces").class)
+        assert_equal(String, string_return("Coded with $pecial").class)
+        assert_equal(String, string_return("Coded with .>?<?</.").class)
+    
     end
+
+
 end
